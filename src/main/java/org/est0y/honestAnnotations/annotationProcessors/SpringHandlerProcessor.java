24797cda7b1949689d5325198861d20ceb6f6d9b
est0y <asteoon@gmail.com>
2024-05-22 04:38:01 +0300

    refactor

--- src/main/java/org/est0y/honestAnnotations/annotationProcessors/SpringHandlerProcessor.java
+++ src/main/java/org/est0y/honestAnnotations/annotationProcessors/SpringHandlerProcessor.java
@@ -2,77 +2,54 @@ package org.est0y.honestAnnotations.annotationProcessors;
 
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import org.est0y.honestAnnotations.annotations.SpringHandler;
-import org.est0y.honestAnnotations.annotationsTools.AnnotationFinder;
-import org.est0y.honestAnnotations.annotationsTools.NestedAnnotations;
-import org.est0y.honestAnnotations.exceptions.BeanNotFound;
-import org.est0y.honestAnnotations.handlers.AnnotationHandler;
 import lombok.extern.slf4j.Slf4j;
-
+import org.est0y.honestAnnotations.annotations.SpringHandler;
+import org.est0y.honestAnnotations.annotationsTools.OrderedHonestAnnotationsHolder;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.ApplicationContext;
+import org.springframework.core.Ordered;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.lang.annotation.Annotation;
+import java.util.NavigableSet;
 
 @Component
 @Slf4j
 @RequiredArgsConstructor
-public class SpringHandlerProcessor implements BeanPostProcessor {
+public class SpringHandlerProcessor implements BeanPostProcessor, Ordered {
 
     private final ApplicationContext applicationContext;
 
-    private final NestedAnnotations nestedAnnotations;
-
-    private final AnnotationFinder annotationFinder;
-
-    private final Map<String, List<String>> annotationHandlers = new HashMap<>();
-
+    private final OrderedHonestAnnotationsHolder orderedHonestAnnotationsHolder;
 
     @Override
     @Nullable
     public Object postProcessBeforeInitialization(@NonNull Object bean, @NonNull String beanName) throws BeansException {
-        var beanAnnotations = annotationFinder.getAllAnnotations(bean);
-
-        for (var annotation : beanAnnotations) {
-            var nestedAnnotationsList = nestedAnnotations.getNestedAnnotation(annotation, SpringHandler.class);
-            if (!nestedAnnotationsList.isEmpty()) {
-                annotationHandlers.putIfAbsent(beanName, new ArrayList<>());
-            }
-            for (SpringHandler springHandler : nestedAnnotationsList) {
-                Class<? extends AnnotationHandler> handlerClass = springHandler.value();
-                String handleBeanName;
-                try {
-                    handleBeanName = applicationContext.getBeanNamesForType(handlerClass)[0];
-                } catch (ArrayIndexOutOfBoundsException e) {
-                    throw new BeanNotFound(String.format("with class %s", handlerClass.getName()), e);
-                }
-                annotationHandlers.get(beanName).add(handleBeanName);
-            }
-        }
         return bean;
     }
 
     @Override
     @Nullable
-    public Object postProcessAfterInitialization(@NonNull Object bean,@NonNull  String beanName) throws BeansException {
-        var handlersBeanNames = annotationHandlers.get(beanName);
-        if (handlersBeanNames != null) {
-            for (String handlerBeanName : handlersBeanNames) {
-                var handler = applicationContext.getBean(handlerBeanName);
-                try {
-                    var method = handler.getClass().getMethod("handle", Object.class, String.class);
-                    bean = method.invoke(handler, bean, beanName);
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
-            }
+    public Object postProcessAfterInitialization(@NonNull Object bean, @NonNull String beanName) throws BeansException {
+        NavigableSet<Annotation> honestAnnotations = orderedHonestAnnotationsHolder.getAnnotations(beanName);
+        if (honestAnnotations == null) {
+            return bean;
+        }
+        if (honestAnnotations.isEmpty()) {
+            return bean;
+        }
+        for (Annotation honestAnnotation : honestAnnotations) {
+            SpringHandler springHandler = honestAnnotation.annotationType().getAnnotation(SpringHandler.class);
+            var handler = applicationContext.getBean(springHandler.value());
+            bean = handler.handle(bean, beanName);
         }
         return bean;
+    }
+
+    @Override
+    public int getOrder() {
+        return Integer.MIN_VALUE;
     }
 }
