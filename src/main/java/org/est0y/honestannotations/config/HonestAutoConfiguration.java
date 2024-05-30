27485193e83e14a535dd06106b006bbf56fae253
est0y <asteoon@gmail.com>
2024-05-30 06:02:01 +0300

    delete spring boot from compile dependencies

--- src/main/java/org/est0y/honestannotations/config/HonestAutoConfiguration.java
+++ src/main/java/org/est0y/honestannotations/config/HonestAutoConfiguration.java
@@ -1,4 +1,4 @@
-package org.est0y.honestannotations;
+package org.est0y.honestannotations.config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.est0y.honestannotations.annotationProcessors.BeanFactoryPostProcessorResolver;
@@ -6,7 +6,6 @@ import org.est0y.honestannotations.annotationProcessors.SpringHandlerProcessor;
 import org.est0y.honestannotations.annotationsTools.AnnotationFinder;
 import org.est0y.honestannotations.annotationsTools.NestedAnnotations;
 import org.est0y.honestannotations.annotationsTools.OrderedHonestAnnotationsHolder;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,25 +15,21 @@ import org.springframework.context.annotation.Configuration;
 public class HonestAutoConfiguration {
 
      @Bean
-     @ConditionalOnMissingBean
     public AnnotationFinder annotationFinder() {
         return new AnnotationFinder();
     }
 
     @Bean
-    @ConditionalOnMissingBean
     public NestedAnnotations nestedAnnotations() {
         return new NestedAnnotations();
     }
 
     @Bean
-    @ConditionalOnMissingBean
     public BeanFactoryPostProcessorResolver beanFactoryPostProcessorResolver() {
         return new BeanFactoryPostProcessorResolver();
     }
 
     @Bean
-    @ConditionalOnMissingBean
     public SpringHandlerProcessor springHandlerProcessor(ApplicationContext applicationContext,
                                                          OrderedHonestAnnotationsHolder holder) {
         return new SpringHandlerProcessor(applicationContext, holder);
